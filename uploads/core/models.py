b2c5544a9ac88d6da1d4dedc73f5e83704cffd45
sh <nasak8640@gmail.com>
2017-11-19 16:28:52 +0900

    change test

--- uploads/core/models.py
+++ uploads/core/models.py
@@ -7,3 +7,6 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    record_ip = models.GenericIPAddressField(blank=True, null=True)
+
+
